39a342c304d6afc8e3400822eabb5d0098e3daf4
s.kotelevsky <s.kotelevsky@sfbtech.org>
2026-03-11 16:05:42 +0300

    Карточка картинки правки

--- backend/app/Http/Controllers/Api/V1/App/Image/ImageController.php
+++ backend/app/Http/Controllers/Api/V1/App/Image/ImageController.php
@@ -3,20 +3,11 @@
 namespace App\Http\Controllers\Api\V1\App\Image;
 
 use App\Http\Controllers\Controller;
-use App\Http\Filters\ImagesFilter;
-use App\Http\Requests\Admin\Image\CreateImageRequest;
-use App\Http\Requests\Admin\Image\DeleteImageRequest;
-use App\Http\Requests\Admin\Image\UpdateImageDateRequest;
-use App\Http\Requests\Admin\Tag\AttachTagsToImageRequest;
-use App\Http\Resources\Admin\Category\ImageCollection;
-use App\Http\Resources\Admin\Category\ImageResource;
+use App\Http\Resources\App\Image\ImageItemResource;
+use App\Http\Resources\App\Image\ImageResource;
 use App\Services\Category\ImageService;
-use App\Services\FaceDetected\FaceDetectedService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\ValidationException;
-use Intervention\Image\Facades\Image;
 use OpenApi\Attributes as OA;
 
 #[OA\Tag(name: 'Images')]
@@ -40,7 +31,7 @@ class ImageController extends Controller
                 description: 'Successful operation',
                 content: new OA\JsonContent(
                     type: 'array',
-                    items: new OA\Items(ref: '#/components/schemas/ImageResource')
+                    items: new OA\Items(ref: '#/components/schemas/AppImageResource')
                 )
             ),
             new OA\Response(
@@ -60,7 +51,6 @@ class ImageController extends Controller
     ): JsonResponse
     {
         try {
-
             $images = $imageService->getByCategoryId($categoryId);
 
         } catch (\Exception $exception){
@@ -70,4 +60,49 @@ class ImageController extends Controller
         return response()->json(ImageResource::collection($images));
     }
 
+    #[OA\Get(
+        path: '/category/{categoryId}/images/{imageId}',
+        summary: 'Получаем изображения категории',
+        parameters: [
+            new OA\Parameter(
+                name: 'categoryId',
+                description: 'ID категории',
+                in: 'path',
+                required: true,
+                schema: new OA\Schema(type: 'integer')
+            ),
+            new OA\Parameter(
+                name: 'imageId',
+                description: 'ID изображения',
+                in: 'path',
+                required: true,
+                schema: new OA\Schema(type: 'integer')
+            )
+        ],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'Successful operation',
+                content: new OA\JsonContent(
+                    type: 'array',
+                    items: new OA\Items(ref: '#/components/schemas/AppImageItemResource')
+                )
+            ),
+            new OA\Response(
+                response: 422,
+                description: 'Validation error',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(property: 'message', type: 'string', example: 'Error message')
+                    ]
+                )
+            )
+        ]
+    )]
+    public function show(int $id, ImageService $imageService): JsonResponse
+    {
+        $image = $imageService->find($id);
+
+        return response()->json(ImageItemResource::make($image));
+    }
 }
